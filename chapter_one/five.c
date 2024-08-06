44b124fdc91b4cc35e3d6a7cafe34ab599df8660
Jon Edgeworth <jedgeworth@echofive.net>
2024-08-06 18:44:25 -0400

    +section 1.5

---
+++ chapter_one/five.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* copy input to output; 1st version */
+
+int main(void) {
+  print("1.5.1 - File Copying")
+  int c;
+
+  c = getchar();
+  while (c != EOF) {
+    putchar(c);
+    c = getchar();
+  }
+}
+
+/* copy input to output; 2nd version */
+
+int main(void) {
+  int c;
+
+  while ((c = getchar()) != EOF)
+    putchar(c);
+}
